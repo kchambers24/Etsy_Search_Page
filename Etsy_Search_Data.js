3e4443e54c3935781fd7b9a4c2ff59a46710ff5e
Kevin Chambers <kbchambers2@gmail.com>
2016-08-10 00:35:24 -0400

    Add files via upload

---
+++ Etsy_Search_Data.js
@@ -0,0 +1,43 @@
+(function($) {
+
+    $(document).ready(function() {
+        $('#etsy-search').bind('submit', function() {
+            api_key = "gkcbqfnmjco5liki7ugqi2j1";
+            terms = $('#etsy-terms').val();
+            etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords=" +
+                terms + "&limit=12&includes=Images:1&api_key=" + api_key;
+
+            $('#etsy-images').empty();
+            $('<p></p>').text('Searching for ' + terms).appendTo('#etsy-images');
+
+            $.ajax({
+                url: etsyURL,
+                dataType: 'jsonp',
+                success: function(data) {
+                    if (data.ok) {
+                        $('#etsy-images').empty();
+                        if (data.count > 0) {
+                            $.each(data.results, function(i, item) {
+                                $("<img/>").attr("src", item.Images[0].url_170x135).appendTo("#etsy-images").wrap(
+                                    "<a href='" + item.url + "'></a>"
+                                );
+                                if (i % 4 == 3) {
+                                    $('<br/>').appendTo('#etsy-images');
+                                }
+                            });
+                        } else {
+                            $('<p>No results.</p>').appendTo('#etsy-images');
+                        }
+                    } else {
+                        $('#etsy-images').empty();
+                        alert(data.error);
+                    }
+                }
+            });
+
+            return false;
+        })
+    });
+
+
+})(jQuery);
